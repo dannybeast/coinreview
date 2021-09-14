389bb2d4b8b2d4029a5302ef773669cbeca8a542
dannybeastrus <dannybeastrus@gmail.com>
2021-09-14 19:25:54 +0300

    Search

--- src/index.js
+++ src/index.js
@@ -28,8 +28,111 @@ import "./js/libs/datepicker";
 import "./js/modules/customSelect";
 import copy from "./js/modules/copy";
 
-
 $(document).ready(function(){
+
+  let $autocompleteInput = $( ".js-search-autocomplete" );
+  let delayTime = 500
+  let autocompleteUrl = 'https://jsonplaceholder.typicode.com/todos'
+  let autocompleteLength = 2
+
+  $autocompleteInput.parent().append(`<div class="search-dropdown"></div>`)
+  $autocompleteInput.on('keyup', delay(function(e) {
+    let val = $(this).val();
+    let that = $(this)
+    let dropdown = that.parents('.search-module__field').find('.search-dropdown')
+      if(val.length >= autocompleteLength){
+        $.ajax( {
+          type: 'POST',
+          url: autocompleteUrl,
+          data: {
+            term: val
+          },
+          beforeSend: function (data) {
+            dropdown.html('')
+            dropdown.append(`<div class="search-item-loader"></div>`)
+          },
+          success: function( data ) {
+            let res = [
+              {
+                id: 43534,
+                currency: 'Bitcoin',
+                link: 'coin.html',
+                code: 'BTC',
+                price: 34.394,
+                price_change: 0.23
+              },
+              {
+                id: 43535,
+                currency: 'Etherium',
+                link: 'coin.html',
+                code: 'ETH',
+                price: 4.394,
+                price_change: -1.23
+              },
+              {
+                id: 43536,
+                currency: 'Dai',
+                link: 'coin.html',
+                code: 'DAI',
+                price: 34.394,
+                price_change: 0.23
+              },
+            ];
+  
+            dropdown.html('')
+  
+            res.forEach(element => {
+              let priceChangeClass = "color-danger"
+              if(element.price_change && element.price_change > 0){
+                priceChangeClass = "color-success"
+                element.price_change = '+' + element.price_change
+              }
+  
+              dropdown.append(`
+  
+              <a href="${element.link}" class="search-item">
+  
+                <div class="search-item__col">
+                  <p class="search-item__id">#${element.id}</p>
+                  <p class="search-item__currency">${element.currency} <span>${element.code}</span></p>
+                </div>
+  
+                <div class="search-item__col">
+                  <p class="search-item__price">$${element.price}</p>
+                  <p class="search-item__price_change ${priceChangeClass}">${element.price_change}%</p>
+                </div>
+  
+              </a>
+              
+              `)
+            });            
+  
+          },
+          error: function(xhr) {
+            window.notifier.alert('Error', {labels: {alert: xhr.status}, durations: {alert:3000}})
+          },
+          complete: function (data) {
+            $('.search-item-loader').remove()
+          }
+        });
+      }else{
+        dropdown.html('')
+      }
+
+    }, delayTime));
+
+
+  function delay(callback, ms) {
+    var timer = 0;
+    return function() {
+      var context = this, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        callback.apply(context, args);
+      }, ms || 0);
+    };
+  }
+
 
 
   copy()
@@ -157,9 +260,9 @@ $(document).ready(function(){
   //   $(this).toggleText(enabledText, disableText);
   // })
 
-  $(".table-hover td:not(.not-clickable)").click(function(e) {
-      window.location = $(this).parents('tr').data("href");
-  });
+  // $(".table-hover td:not(.not-clickable)").click(function(e) {
+  //     window.location = $(this).parents('tr').data("href");
+  // });
 
 
 
